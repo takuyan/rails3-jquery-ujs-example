e9c4cac0263092e916f504f0765ff5d568422606
KATO TAKUYA <fanciful.dream@gmail.com>
2011-01-07 22:14:56 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3JqueryUjsExample::Application.config.secret_token = '7cb8ffcede261f76048fffcffe23f597dbe28522e6713f53016005c105d2dd7a48d76b702273b6969f0454c46798ac8305b233a0a24c1482205d6567ba8e3d70'
